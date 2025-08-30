349528c5343bd5f9b811f6bd0dbc292aa752d3ca
Alex Buly <asbuly@icloud.com>
2025-08-30 14:53:02 -0400

    Edit home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,17 @@
+import shopImage from "../images/mike-petrucci-c9FQyqIECds-unsplash.jpg";
+import { Link } from "react-router-dom";
+import '../styles/App.css'
+
 export const Home = () => {
     return (
-        <h3>Hello! Welcome to the home page!</h3>
+        <>
+            <div className="welcome">
+                <h1>Welcome to our shop!</h1>
+                <div className="bottom">
+                     <img src={shopImage} className="shop-image" alt="shop" />
+                     <Link to="shop" className="shop-link">Shop now</Link>
+                </div>
+            </div>
+        </>
     )
 }
