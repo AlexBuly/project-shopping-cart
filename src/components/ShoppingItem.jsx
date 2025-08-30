2499486d42f585a0bbbf14a398be44621ec426a5
Alex Buly <asbuly@icloud.com>
2025-08-30 14:54:46 -0400

    Add add increment, decrement, and add to cart buttons

--- src/components/ShoppingItem.jsx
+++ src/components/ShoppingItem.jsx
@@ -1,13 +1,30 @@
 import '../styles/App.css'
+import { useState } from 'react'
 
-export const ShoppingItem = ({name}) => {
-    // individual item
-    // contains, subtract, add, and description
+export const ShoppingItem = ({name, price}) => {
+    const [count, setCount] = useState(0);
+   
+    const increment = () => {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1);
+    }
+
     return (
         <>
             <div className="shopping-items">
                 <div className="item">
-                    <p>{name}</p>
+                    <p>Name: {name}</p>
+                    <br />
+                    <p>Price: {price}</p>
+                    <div className="number-btns">
+                        <button onClick={decrement}>-</button>
+                        <p>{count}</p>
+                        <button onClick={increment}>+</button>
+                    </div>
+                    <button>Add to Cart</button>
                 </div>
             </div>
         </>
