eef35e9380b52aa0400e4c6472bfd7ddb288c36f
Alex Buly <asbuly@icloud.com>
2025-09-04 15:44:50 -0400

    Add outlet context

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [cartItems, setCartItems] = useState([]);
 
+   const addToCart = (product, count) => {
+    if (count > 0) {
+      setCartItems(prev => [...prev, { ...product, quantity: count }]);
+    }
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar cartCount={cartItems.length}/>
+      <main>
+        <Outlet context={{cartItems, addToCart}} />
+      </main>
+
     </>
   )
 }
