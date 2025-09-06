bc5c8db096076ea49cef5cd49e38b438bc8775dd
Alex Buly <asbuly@icloud.com>
2025-09-06 14:47:28 -0400

    Add error element

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,3 @@
+export const ErrorPage = () => {
+    return "Oops this page doens't exist!"
+}
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,12 +2,14 @@ import { Navigate } from "react-router-dom";
 import { Home } from "../components/Home";
 import { Shop } from "../components/Shop";
 import { Cart } from "../components/Cart";
+import { ErrorPage } from "../components/ErrorPage";
 import App from "../App";
 
 const routes = [
     {
         path: "/",
         element: <App/>,
+        errorElement: <ErrorPage/>,
         children: [
             {
                 index: true,
