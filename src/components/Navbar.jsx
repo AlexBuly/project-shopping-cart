9f16da6438acfb5a6716ddd933fc999739a3dc38
Alex Buly <asbuly@icloud.com>
2025-09-05 22:20:06 -0400

    Add cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
 import '../styles/App.css'
+import Icon from '@mdi/react';
+import { mdiCartOutline } from '@mdi/js';
+
 
 export const Navbar = ({cartCount}) => {
     return (
         <nav>
             <Link to="home" className="nav-link">Home</Link>
             <Link to="shop" className="nav-link">Shop</Link>
-            <Link to="cart" className="nav-link">Cart {cartCount}</Link>
+            <Link to="cart" className="nav-link"><Icon path={mdiCartOutline} size={1} /> {cartCount}</Link>
         </nav>
     )
 }
