a1546521a16f91ad67af62a2ba00412eca70e431
Alex Buly <asbuly@icloud.com>
2025-09-05 22:20:40 -0400

    Add additional shopping items

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -11,6 +11,21 @@ export const Shop = () => {
                 <ShoppingItem id={3} addItem={addToCart}/>
                 <ShoppingItem id={4} addItem={addToCart}/>
                 <ShoppingItem id={5} addItem={addToCart}/>
+                <ShoppingItem id={6} addItem={addToCart}/>
+                <ShoppingItem id={7} addItem={addToCart}/>
+                <ShoppingItem id={8} addItem={addToCart}/>
+                <ShoppingItem id={9} addItem={addToCart}/>
+                <ShoppingItem id={10} addItem={addToCart}/>
+                <ShoppingItem id={11} addItem={addToCart}/>
+                <ShoppingItem id={12} addItem={addToCart}/>
+                <ShoppingItem id={13} addItem={addToCart}/>
+                <ShoppingItem id={14} addItem={addToCart}/>
+                <ShoppingItem id={15} addItem={addToCart}/>
+                <ShoppingItem id={16} addItem={addToCart}/>
+                <ShoppingItem id={17} addItem={addToCart}/>
+                <ShoppingItem id={18} addItem={addToCart}/>
+                <ShoppingItem id={19} addItem={addToCart}/>
+                <ShoppingItem id={20} addItem={addToCart}/>
             </div>
 
         </>
